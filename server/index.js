c65ca6809db0e93f7a9f9e510ac2e069b15696a7
yu chiu <yuchiu2002@hotmail.com>
2018-07-03 15:46:20 -0400

    setup models

--- server/index.js
+++ server/index.js
@@ -1,21 +1,22 @@
 import express from 'express';
-import bodyParser from 'body-parser'
-import {graphqlExpress, graphiqlExpress} from 'apollo-server-express'
-import typeDefs from './schema'
-import resolvers from './resolvers'
+import bodyParser from 'body-parser';
+import { graphqlExpress, graphiqlExpress } from 'apollo-server-express';
+import { makeExecutableSchema } from 'graphql-tools';
+import typeDefs from './schema';
+import resolvers from './resolvers';
+import models from './models';
 
-import {makeExecutableSchema} from 'graphql-tools'
 
-export const schema = makeExecutableSchema({typeDefs, resolvers})
+const schema = makeExecutableSchema({ typeDefs, resolvers });
 
 const PORT = 8081;
 
-const app = express()
+const app = express();
 
 const graphqlEndpoint = '/graphql';
 
-app.use(graphqlEndpoint, bodyParser.json(), graphqlExpress({schema}))
+app.use(graphqlEndpoint, bodyParser.json(), graphqlExpress({ schema }));
 
-app.use('/graphiql', graphiqlExpress({endpointURL: graphqlEndpoint}))
+app.use('/graphiql', graphiqlExpress({ endpointURL: graphqlEndpoint }));
 
-app.listen(PORT)
+app.listen(PORT);
---
+++ server/models/message.js
@@ -0,0 +1,14 @@
+export default (sequelize, DataTypes) => {
+  const Message = sequelize.define('message', {
+    text: DataTypes.STRING,
+  });
+  Message.associate = (models) => {
+    Message.belongsTo(models.Channel, {
+      foreignKey: 'channerlId',
+    });
+    Message.belongsTo(models.User, {
+      foreignKey: 'userId',
+    });
+  };
+  return Message;
+};
